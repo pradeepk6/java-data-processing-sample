1626e59e8ea671ef1573b2bf2c6d8ac0ef2a4379
pradeepk6 <pradeepk6@outlook.com>
2019-07-07 00:55:29 +0100

    added line/data validation and exceptions

--- src/main/java/com/connectgroup/DataFilterer.java
+++ src/main/java/com/connectgroup/DataFilterer.java
@@ -6,6 +6,7 @@ import java.io.Reader;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class DataFilterer {
@@ -15,9 +16,12 @@ public class DataFilterer {
         try (BufferedReader br = new BufferedReader(source)) {
             list = br.lines()
                     .skip(1)
+                    .map(DataFilterer::validateLine)
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
                     .filter(line -> {
                         String[] tokens = line.split(",");
-                         return (tokens[1].equalsIgnoreCase(country));
+                        return (tokens[1].equalsIgnoreCase(country));
                     })
                     .collect(Collectors.toList());
         } catch (IOException e) {
@@ -30,12 +34,15 @@ public class DataFilterer {
         List list = Collections.emptyList();
         try (BufferedReader br = new BufferedReader(source)) {
             list = br.lines()
-                     .skip(1)
-                     .filter(line -> {
+                    .skip(1)
+                    .map(DataFilterer::validateLine)
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .filter(line -> {
                         String[] tokens = line.split(",");
                         return ( tokens[1].equalsIgnoreCase(country) && Long.parseLong(tokens[2])>limit );
-                      })
-                     .collect(Collectors.toList());
+                    })
+                    .collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -45,18 +52,19 @@ public class DataFilterer {
     public static Collection<?> filterByResponseTimeAboveAverage(Reader source) {
         List list = Collections.emptyList();
         try( BufferedReader br = new BufferedReader(source)) {
-            List<String> dataList = br.lines().skip(1).collect(Collectors.toList());
+            List<String> dataList = br.lines()
+                    .skip(1)
+                    .map(DataFilterer::validateLine)
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .collect(Collectors.toList());
+
             double avg = dataList.stream()
-                                 .mapToLong(line -> {
-                                    String[] tokens = line.split(",");
-                                    return Long.decode(tokens[2]);
-                                 })
-                                .average().getAsDouble();
+                    .mapToLong(line -> Long.parseLong(line.split(",")[2]))
+                    .average().orElseThrow(() -> new IllegalArgumentException("Error calculating average"));
+
             list = dataList.stream()
-                    .filter(line -> {
-                        String[] tokens = line.split(",");
-                        return Long.parseLong(tokens[2]) > avg;
-                     })
+                    .filter(line -> Long.parseLong(line.split(",")[2]) > avg)
                     .collect(Collectors.toList());
 
         } catch (IOException e) {
@@ -64,4 +72,18 @@ public class DataFilterer {
         }
         return list;
     }
+
+    // if line is invalid return null
+    private static Optional<String> validateLine(String line) {
+        try {
+            String[] tokens = line.split(",");
+            if (tokens.length < 3) throw new Exception("Num of cols is less than 3 for line : " + line);
+            Long.parseLong(tokens[2]); // make sure col2 is a number
+        } catch (Exception e) {
+            System.out.println("Invalid line : " + line + " : " + e.getMessage());
+            line = null;
+        }
+        return Optional.ofNullable(line);
+    }
+
 }
